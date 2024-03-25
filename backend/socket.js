56d420b48a5aad1be3b38d5225122df322a865af
Shubham Shinde <shubhamshinde6762@gmail.com>
2024-03-25 15:54:06 +0530

    origin changed

--- backend/socket.js
+++ backend/socket.js
@@ -9,7 +9,7 @@ exports.initSocket = (app) => {
     const server = createServer(app);
     io = new Server(server, {
       cors: {
-        origin: "https://collabo-hub-qwzu.vercel.app/",
+        origin: "http://192.168.234.48:3000/",
       },
     });
 
