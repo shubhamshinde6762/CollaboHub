43f7fd464d512f84feb6f39d52ff8128458be6a1
Shubham Shinde <shubhamshinde6762@gmail.com>
2024-03-25 10:48:54 +0530

    vercel added

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,17 @@ const { dbConnect } = require("./db/db.js");
 const { cdnConnect } = require("./db/cdn.js");
 const { initSocket } = require("./socket.js");
 
-const fetchChatData = require("./routes/ChatHandler/fetchChatData.js");
-const sendMessage = require("./routes/ChatHandler/sendMessage.js");
+// const fetchChatData = require("./routes/ChatHandler/fetchChatData.js");
+// const sendMessage = require("./routes/ChatHandler/sendMessage.js");
 
-const editProject = require("./routes/ProjectFunctions/editProject.js");
-const fetchProject = require("./routes/ProjectFunctions/fetchProject.js");
-const invite = require("./routes/ProjectFunctions/invite.js");
+// const editProject = require("./routes/ProjectFunctions/editProject.js");
+// const fetchProject = require("./routes/ProjectFunctions/fetchProject.js");
+// const invite = require("./routes/ProjectFunctions/invite.js");
 
-const taskHandler = require("./routes/TaskFunctions/taskHandler.js");
-const authentication = require("./routes/User/authentication.js");
-const updateUser = require("./routes/User/updateUser.js");
-const note = require("./routes/User/notes.js");
+// const taskHandler = require("./routes/TaskFunctions/taskHandler.js");
+// const authentication = require("./routes/User/authentication.js");
+// const updateUser = require("./routes/User/updateUser.js");
+// const note = require("./routes/User/notes.js");
 
 require("dotenv").config();
 const app = express();
