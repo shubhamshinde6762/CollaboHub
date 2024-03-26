6b9146da73e5cb37715df532ce8ecb7e375e5823
Shubham Shinde <shubhamshinde6762@gmail.com>
2024-03-26 17:49:19 +0530

    origin changed

--- main/src/components/DashBoardComponents/ActionPanel.js
+++ main/src/components/DashBoardComponents/ActionPanel.js
@@ -41,7 +41,7 @@ const ActionPanel = (props) => {
     try {
       console.log(props.user);
       const response = await axios.post(
-        "http://13.210.25.126:5000/api/v1/getProjects",
+        "https://13.210.25.126:5000/api/v1/getProjects",
         {
           email: props.user.data.email,
         }
