bcd79798bb7a8dbe9b086ef8bf3300e1d0638daf
Shubham Shinde <shubhamshinde6762@gmail.com>
2024-04-12 16:01:30 +0530

    last updated

--- main/src/components/LandingPage/Footer.js
+++ main/src/components/LandingPage/Footer.js
@@ -4,12 +4,13 @@ import { FaGithub } from "react-icons/fa";
 import { RiTwitterXFill } from "react-icons/ri";
 
 import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 // import { CiLinkedin, FaGithub, RiTwitterXFill } from "react-icons/all";
 
 const Footer = () => {
   return (
     <motion.div
-    id="about"
+      id="about"
       className="from-violet-600 to-purple-500 pt-12  bg-gradient-to-b text-white p-8 w-full flex flex-wrap justify-evenly items-start gap-6"
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
@@ -36,15 +37,24 @@ const Footer = () => {
             whileHover={{ scale: 1.1 }}
           >
             <img
-              src="vcdv"
-              className="w-[6vw] h-[6vw] rounded-full bg-black aspect-auto"
+              src="https://media.licdn.com/dms/image/D4D03AQF6isUbW2Up_w/profile-displayphoto-shrink_400_400/0/1708968816185?e=1718236800&v=beta&t=EOprmMjgYlupWMh3DX0OX-GX2l-xsAxnHL2aNHSpP3s"
+              className="w-[6vw] min-w-[70px] rounded-full bg-black aspect-square"
             />
             <p className="font-bold font-poppins">Shubham Shinde</p>
             <p className="font-bold text-xs font-lato">Full Stack Developer</p>
             <div className="flex justify-center items-center gap-2 text-2xl my-2">
-              <CiLinkedin className="cursor-pointer" />
-              <FaGithub className="cursor-pointer" />
-              <RiTwitterXFill className="cursor-pointer" />
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/shubham-shinde-1061a8252/"
+              >
+                <CiLinkedin className="cursor-pointer" />
+              </a>
+              <a target="_blank" href="https://github.com/shubhamshinde6762">
+                <FaGithub className="cursor-pointer" />
+              </a>
+              <a>
+                <RiTwitterXFill className="cursor-pointer" />
+              </a>
             </div>
           </motion.div>
           <motion.div
@@ -52,15 +62,24 @@ const Footer = () => {
             whileHover={{ scale: 1.1 }}
           >
             <img
-              src="vcdv"
-              className="w-[6vw] h-[6vw] rounded-full bg-black aspect-auto"
+              src="https://media.licdn.com/dms/image/D4D03AQGYPO70nXy6Bg/profile-displayphoto-shrink_400_400/0/1712917294515?e=1718236800&v=beta&t=cyw2Y9nIVx-81mLgOU-iBwZoUGoXa0v9VU_riFiVeT4"
+              className="w-[6vw]  min-w-[70px] rounded-full bg-black aspect-square"
             />
             <p className="font-bold font-poppins">Pawan Kusekar</p>
             <p className="font-bold text-xs font-lato">Full Stack Developer</p>
             <div className="flex justify-center items-center gap-2 text-2xl my-2">
-              <CiLinkedin className="cursor-pointer" />
-              <FaGithub className="cursor-pointer" />
-              <RiTwitterXFill className="cursor-pointer" />
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/pawan-kusekar-677050265/"
+              >
+                <CiLinkedin className="cursor-pointer" />
+              </a>
+              <a target="_blank" href="https://github.com/pawank505">
+                <FaGithub className="cursor-pointer" />
+              </a>
+              <a>
+                <RiTwitterXFill className="cursor-pointer" />
+              </a>
             </div>
           </motion.div>
         </div>
@@ -70,4 +89,3 @@ const Footer = () => {
 };
 
 export default Footer;
-  
